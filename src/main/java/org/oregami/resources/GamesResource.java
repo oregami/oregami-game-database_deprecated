d07f960d6c24cbe8d3a3751c3399acd23f03366a
kultpower <gene@kultpower.de>
2013-09-15 13:48:47 +0200

    urls, preparation of PUT method

--- src/main/java/org/oregami/resources/GamesResource.java
+++ src/main/java/org/oregami/resources/GamesResource.java
@@ -2,11 +2,18 @@ package org.oregami.resources;
 
 import java.util.List;
 
+import javax.persistence.OptimisticLockException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
 import org.oregami.data.DatabaseFiller;
 import org.oregami.data.GameDao;
 import org.oregami.entities.Game;
@@ -16,6 +23,7 @@ import com.google.inject.Inject;
 
 @Path("/games")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GamesResource {
 
 	public GameDao gameRepository;
@@ -30,7 +38,6 @@ public class GamesResource {
 	
 	  
 	@GET
-	@Path("/list")
 	public List<Game> list() {
 		List<Game> ret = null;
 		gameRepository.getTransaction().begin();
@@ -42,24 +49,50 @@ public class GamesResource {
 		return ret;
 	}
 	
-	
-	@GET
-	@Path("/init")
-	public String init() {
-		String ret = "data was already there";
+	@POST
+	public Response addGame(Game newGame) {
+		System.out.println("post: " + newGame);
 		gameRepository.getTransaction().begin();
-		Game game = null;
-		game = gameRepository.findOne(1L);
-		if (game==null) {
-			getDatabaseFiller().initData();
-			ret = "data initialized";
-		}
+		gameRepository.save(newGame);
 		gameRepository.getTransaction().commit();
-		
-		return ret;
+		return Response.status(Response.Status.CREATED).entity(newGame).build();
 	}
+	
+	
+	@PUT
+	public Response updateGame(Game updatedGame) {
+		if (updatedGame.getId()==null) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
+		try {
+			
+		System.out.println("put: " + updatedGame);
+//		gameRepository.getTransaction().begin();
+		gameRepository.update(updatedGame);
+		
+		} catch (OptimisticLockException e) {
+			Logger.getLogger(this.getClass()).warn("OptimisticLockException", e);
+			return Response.status(Response.Status.BAD_REQUEST).tag("OptimisticLockException").build();
+		}
+		finally {
+//			gameRepository.getTransaction().commit();
+		}
+		return Response.status(Response.Status.ACCEPTED).entity(updatedGame).build();
+	}	
 
 
+    @GET
+    @Path("/{id}")
+	public Response getGame(@PathParam("id") long id) {
+    	Game game = gameRepository.findOne(id);
+    	if (game!=null) {
+    		return Response.ok(game).build();
+    	} else {
+    		return Response.status(Response.Status.NOT_FOUND).build();
+    	}
+	}
+	
+	
 	private DatabaseFiller getDatabaseFiller() {
 		return databaseFiller;
 	}	
