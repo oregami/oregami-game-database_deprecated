51beb1e55b85c363752b9d9ae023043d6814a86c
kultpower <gene@kultpower.de>
2013-11-25 23:41:53 +0100

    db tests with rollback

--- src/test/java/org/oregami/test/PersistenceTest.java
+++ src/test/java/org/oregami/test/PersistenceTest.java
@@ -1,8 +1,12 @@
 package org.oregami.test;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.oregami.data.GameDao;
@@ -11,6 +15,7 @@ import org.oregami.entities.Game;
 import org.oregami.entities.GameTitle;
 import org.oregami.entities.ReleaseGroup;
 import org.oregami.entities.datalist.GameEntryType;
+import org.oregami.entities.datalist.ReleaseType;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -22,6 +27,8 @@ public class PersistenceTest {
 
 	private static Injector injector;
 	
+	EntityManager entityManager = null;
+	
 	public PersistenceTest() {
 	}
 	
@@ -32,6 +39,19 @@ public class PersistenceTest {
 		injector.getInstance(PersistenceTest.class);
 		PersistService persistService = injector.getInstance(PersistService.class);
 		persistService.start();
+	}
+	
+	@Before
+	public void startTx() {
+		if (entityManager==null) {
+			entityManager = injector.getInstance(EntityManager.class);
+		}
+		entityManager.getTransaction().begin();
+	}
+	
+	@After
+	public void rollbackTx() {
+		entityManager.getTransaction().rollback();
 	}
 	
 	@Test
@@ -65,44 +85,61 @@ public class PersistenceTest {
 	@Test
 	@Transactional
 	public void testSaveGameWithGameEntryType() {
+		
 		GameEntryType gameEntryType = new GameEntryType();
 		gameEntryType.setValue(GameEntryType.GAME);
 		GameEntryTypeDao gameEntryTypeDao = injector.getInstance(GameEntryTypeDao.class);
 		gameEntryTypeDao.save(gameEntryType);
 		
+		GameEntryType loadedGameEntryType = gameEntryTypeDao.findOne(gameEntryType.getId());
+		Assert.assertNotNull(loadedGameEntryType);
+		Assert.assertEquals(loadedGameEntryType.getValue(), gameEntryType.getValue());
+		
+		GameDao gameDao = injector.getInstance(GameDao.class);
+		
 		Game game = new Game();
 		game.addGameTitle(new GameTitle("Monkey Island"));
 		game.setGameEntryType(gameEntryType);
-		
-		GameDao gameDao = injector.getInstance(GameDao.class);
 		Long gameId = gameDao.save(game);
 		Assert.assertNotNull(gameId);
+
+		Game game2 = new Game();
+		game2.addGameTitle(new GameTitle("Street Fighter"));
+		game2.setGameEntryType(gameEntryType);
+		Long gameId2 = gameDao.save(game2);
+		Assert.assertNotNull(gameId2);
 		
 		Game loadedGame = gameDao.findOne(gameId);
 		Assert.assertNotNull(loadedGame);
 		Assert.assertEquals(game.getGameEntryType(), loadedGame.getGameEntryType());
 
+		Game loadedGame2 = gameDao.findOne(gameId2);
+		Assert.assertNotNull(loadedGame2);
+		Assert.assertEquals(game2.getGameEntryType(), loadedGame2.getGameEntryType());
+		
+		List<Game> allGames = gameDao.findAll();
+		Assert.assertEquals(2, allGames.size());
+		
 		
 	}
 	
 	@Test
-	@Transactional
 	public void testSaveReleaseGroup() {
+		
+		ReleaseType releaseType = new ReleaseType();
+		releaseType.setValue(ReleaseType.NATIVE_DEVELOPMENT);
+		entityManager.persist(releaseType);
+		
 		ReleaseGroup releaseGroup = new ReleaseGroup();
-//		releaseGroup.setReleaseType(ReleaseType.NATIVE_DEVELOPMENT);
-		EntityManager entityManager = injector.getInstance(EntityManager.class);
-		entityManager.getTransaction().begin();
+		releaseGroup.setReleaseType(releaseType);
 		entityManager.persist(releaseGroup);
-		entityManager.flush();
-		entityManager.getTransaction().commit();
 		Long id = releaseGroup.getId();
 		Assert.assertNotNull(id);
 		
-		entityManager.getTransaction().begin();
 		ReleaseGroup rgLoaded = entityManager.find(ReleaseGroup.class, id);
-		entityManager.getTransaction().commit();
 		Assert.assertNotNull(rgLoaded);
 		Assert.assertEquals(rgLoaded.getReleaseType(), releaseGroup.getReleaseType());
+		System.out.println(rgLoaded);
 		
 	}
 	
