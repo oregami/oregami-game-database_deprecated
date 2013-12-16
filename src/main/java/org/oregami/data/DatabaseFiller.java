deab6f2ce89ba4d9c634d59991cc33a8208a04fc
kultpower <gene@kultpower.de>
2013-12-16 21:50:42 +0100

    new test data

--- src/main/java/org/oregami/data/DatabaseFiller.java
+++ src/main/java/org/oregami/data/DatabaseFiller.java
@@ -3,14 +3,16 @@ package org.oregami.data;
 import org.oregami.dropwizard.OregamiService;
 import org.oregami.entities.Game;
 import org.oregami.entities.GameTitle;
+import org.oregami.entities.KeyObjects.SystemKey;
+import org.oregami.entities.ReleaseGroup;
 import org.oregami.entities.datalist.DemoContentType;
 import org.oregami.entities.datalist.GameEntryType;
+import org.oregami.entities.datalist.ReleaseType;
 
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.persist.PersistService;
-import com.google.inject.persist.Transactional;
 import com.google.inject.persist.jpa.JpaPersistModule;
 
 /**
@@ -30,6 +32,9 @@ public class DatabaseFiller {
 	private GameEntryTypeDao gameEntryTypeDao;
 
 	@Inject
+	private ReleaseTypeDao releaseTypeDao;
+	
+	@Inject
 	private DemoContentTypeDao demoContentTypeDao;
 	
 	BaseListFinder baseListFinder = BaseListFinder.instance();
@@ -47,7 +52,6 @@ public class DatabaseFiller {
 		return instance;
 	}
 
-	@Transactional
 	public void initGameEntryType() {
 		gameEntryTypeDao.save(new GameEntryType(GameEntryType.ADD_ON_NOT_SIGNIFICANT));
 		gameEntryTypeDao.save(new GameEntryType(GameEntryType.ADD_ON_SIGNIFICANT));
@@ -57,7 +61,6 @@ public class DatabaseFiller {
 		gameEntryTypeDao.save(new GameEntryType(GameEntryType.GAME));
 	}
 	
-	@Transactional
 	public void initDemoContentType() {
 		demoContentTypeDao.save(new DemoContentType(DemoContentType.ABSOLUTE_PLAY_COUNT_LIMIT));
 		demoContentTypeDao.save(new DemoContentType(DemoContentType.ABSOLUTE_PLAY_TIME_LIMIT));
@@ -67,45 +70,30 @@ public class DatabaseFiller {
 		demoContentTypeDao.save(new DemoContentType(DemoContentType.SCORE_CAP));
 		demoContentTypeDao.save(new DemoContentType(DemoContentType.TECH_DEMO));
 		demoContentTypeDao.save(new DemoContentType(DemoContentType.TIME_LIMIT));
+	}
+	
+	public void initReleaseType() {
+		releaseTypeDao.save(new ReleaseType(ReleaseType.NATIVE_DEVELOPMENT));
+		releaseTypeDao.save(new ReleaseType(ReleaseType.EMULATOR_RELEASE));
+		releaseTypeDao.save(new ReleaseType(ReleaseType.PORT));
 	}	
 	
 	private void addMonkeyIsland() {
 		Game gameMonkeyIsland = new Game();
 		
 //		gameMonkeyIsland.setGameEntryType(gameEntryTypeDao.findByName(GameEntryType.GAME));
+		gameMonkeyIsland.setGameEntryType(baseListFinder.getGameEntryType(GameEntryType.GAME));
 		
 		gameMonkeyIsland.addGameTitle(new GameTitle("Monkey Island"));
 		gameMonkeyIsland.addGameTitle(new GameTitle("Monkey Island 1"));
 		gameMonkeyIsland.addGameTitle(new GameTitle("The Secret of Monkey Island"));
+
+		ReleaseGroup rgDos = new ReleaseGroup("DOS-Demo", SystemKey.MSDOS, baseListFinder.getReleaseType(ReleaseType.NATIVE_DEVELOPMENT));
+		gameMonkeyIsland.addReleaseGroup(rgDos);
 		
-//		gameMonkeyIsland.setGameEntryType(baseListFinder.getGameEntryType(GameEntryType.GAME));
-
-//		ReleaseGroup releaseGroupDos = new ReleaseGroup("DOS", SystemKey.MSDOS, ReleaseGroupReason.ORIGINAL);
-//		ReleaseGroup releaseGroupDosDemo = new ReleaseGroup("DOS", SystemKey.MSDOS, ReleaseGroupReason.DEMO_PROMO);
-//		ReleaseGroup releaseGroupDosEnhanced = new ReleaseGroup("DOS (Verbesserte CD-Version)", SystemKey.MSDOS, ReleaseGroupReason.ENHANCED);
-//
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupDos);
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupDosDemo);
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupDosEnhanced);
-//
-//		// ########### Amiga
-//		ReleaseGroup releaseGroupAmiga = new ReleaseGroup("Amiga 500/600 (OCS/ECS)", SystemKey.Amiga, ReleaseGroupReason.ORIGINAL);
-//		ReleaseGroup releaseGroupAmigaDemo = new ReleaseGroup("Amiga 500/600 (OCS/ECS)", SystemKey.Amiga, ReleaseGroupReason.DEMO_PROMO);
-//
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupAmiga);
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupAmigaDemo);
-//
-//		// ########### Atari ST
-//		ReleaseGroup releaseGroupAtariST = new ReleaseGroup("Atari ST", SystemKey.AtariST, ReleaseGroupReason.ORIGINAL);
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupAtariST);
-//
-//		// ########### Apple
-//		ReleaseGroup releaseGroupApple = new ReleaseGroup("Apple Macintosh", SystemKey.AppleMacintosh, ReleaseGroupReason.ORIGINAL);
-//		ReleaseGroup vogAppleSpecial = new ReleaseGroup("Apple Macintosh", SystemKey.AppleMacintosh, ReleaseGroupReason.ENHANCED);
-//
-//		gameMonkeyIsland.addReleaseGroup(releaseGroupApple);
-//		gameMonkeyIsland.addReleaseGroup(vogAppleSpecial);
-
+		rgDos.addDemoContentType(BaseListFinder.instance().getDemoContentType(DemoContentType.CONTENT_LIMIT));
+		rgDos.addDemoContentType(BaseListFinder.instance().getDemoContentType(DemoContentType.TECH_DEMO));
+		
 		gameRepository.save(gameMonkeyIsland);
 		
 		gameRepository.update(gameMonkeyIsland);
@@ -157,7 +145,7 @@ public class DatabaseFiller {
 		
 	}
 	
-	@Transactional
+//	@Transactional
 	public void initData() {
 		initBaseLists();
 		addGames();
@@ -170,6 +158,7 @@ public class DatabaseFiller {
 		if (!initialized) {
 			initGameEntryType();
 			initDemoContentType();
+			initReleaseType();
 			initialized=true;
 		}
 		
