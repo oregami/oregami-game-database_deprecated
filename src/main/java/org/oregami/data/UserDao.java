fdc099fb965bcf729ceee68133546f7bb211dae0
kultpower <gene@kultpower.de>
2014-02-25 21:18:33 +0100

    NoResultException

--- src/main/java/org/oregami/data/UserDao.java
+++ src/main/java/org/oregami/data/UserDao.java
@@ -1,6 +1,7 @@
 package org.oregami.data;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import org.oregami.entities.user.User;
 
@@ -17,9 +18,13 @@ public class UserDao extends GenericDAOImpl<User, Long>{
 	
 	
 	public User findOneByUsername(String name) {
-    	User user = (User) getEntityManager()
-        		.createNativeQuery("SELECT * FROM User u ", User.class).getSingleResult(); 
-        return user;
+		try {
+			User user = (User) getEntityManager()
+					.createNativeQuery("SELECT * FROM User u where lower(u.username) = :value ", User.class).setParameter("value", name.toLowerCase()).getSingleResult(); 
+			return user;
+		} catch (NoResultException e) {
+			return null;
+		}
     }
 
 
