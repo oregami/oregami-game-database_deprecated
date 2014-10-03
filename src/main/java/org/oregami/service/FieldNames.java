1ba8fbb040c8f50b9283f62f83eaa0e3949e157d
sebeichholz <gene@oregami.org>
2014-10-03 20:46:07 +0200

    new field names & messages

--- src/main/java/org/oregami/service/FieldNames.java
+++ src/main/java/org/oregami/service/FieldNames.java
@@ -5,6 +5,10 @@ public interface FieldNames {
 	public static final String USER_EMAIL = "user.email";
 	public static final String USER_PASSWORD = "user.password";
 	public static final String USER_USERNAME = "user.username";
+
+    public static final String PUBLICATIONFRANCHISE_NAME = "publicationFranchise.name";
+    public static final String PUBLICATION_NAME = "publication.name";
+    public static final String PUBLICATIONISSUE_NAMEYEAR = "publicationIssue.issueNameYear";
 	
 	
 }
